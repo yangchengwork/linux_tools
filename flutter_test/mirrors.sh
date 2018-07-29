f2d6d98f097eec8e9e724e4f427e1d7281ff4f92
Gump Yang <gump.yang@gmail.com>
2018-07-29 09:55:17 +0800

    加入国内镜像的脚本

---
+++ flutter_test/mirrors.sh
@@ -0,0 +1,6 @@
+export PUB_HOSTED_URL=https://pub.flutter-io.cn
+export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+# git clone -b dev https://github.com/flutter/flutter.git
+# export PATH="$PWD/flutter/bin:$PATH"
+# cd ./flutter
+# flutter doctor
