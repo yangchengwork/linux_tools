a293f3516f6c3f0aca4f578e53026768de80e96c
Gump Yang <gump.yang@gmail.com>
2018-07-29 10:13:24 +0800

    添加一个Stateful widget

--- flutter_test/gump_test1/lib/main.dart
+++ flutter_test/gump_test1/lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random(); // 新增了这一行
+    // final wordPair = new WordPair.random(); // 新增了这一行
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -15,9 +15,24 @@ class MyApp extends StatelessWidget {
         ),
         body: new Center(    // 这里把之前的 "const" 换成了 "new".
           //child: const Text('Hello World'),   // 我们不用这样的方式生成文字了
-          child: new Text(wordPair.asPascalCase),  // 这是新的文字生成方式
+          // child: new Text(wordPair.asPascalCase),  // 这是新的文字生成方式
+          child: new RandomWords(),                 // 修改成本行代码
         ),
       ),
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  // TODO Add build method
+  @override                                  // 新增代码片段 - 开始 ... 
+  Widget build(BuildContext context) {
+    final WordPair wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
+  }                                          // ... 新增的代码片段 - 结束
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
