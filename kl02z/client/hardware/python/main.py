f5294468c85f76f399561dce76bd19f583727620
gumpyang <gumpyang@4b97ce20-0248-4398-a9e2-2c177b3f730e>
2018-08-01 15:25:37 +0000

    初步可以加入线程
    git-svn-id: svn://www.svn999.com/gumpyang.learn@3 4b97ce20-0248-4398-a9e2-2c177b3f730e

--- kl02z/client/hardware/python/main.py
+++ kl02z/client/hardware/python/main.py
@@ -2,15 +2,42 @@
 # -*- coding: utf-8 -*
 
 import serial;
+import time;
+# import threading;
+import _thread; 
 
 COMPATH = "/dev/ttyACM0";
 COMBPS = 115200;
 
+class MSerialPort:
+	message=''
+	def __init__(self,port,buand):
+		self.port=serial.Serial(port,buand)
+		if not self.port.isOpen():
+			self.port.open()
+	def port_open(self):
+		if not self.port.isOpen():
+			self.port.open()
+	def port_close(self):
+		self.port.close()
+	def send_data(self,data):
+		number=self.port.write(data)
+		return number
+	def read_data(self):
+		while True:
+			data=self.port.readline()
+			self.message=data
+
 def main():
-    ser = serial.Serial(COMPATH, COMBPS, timeout=0.5);
-    for i in range(0, 10):
-        print(ser.readline());
-    ser.close;
+	mSerial=MSerialPort(COMPATH, COMBPS)
+	_thread.start_new_thread(mSerial.read_data,())
+    # thread_read = threading.Thread(target=mSerial.read_data)
+    # thread_read.start();
+	while True:
+		# time.sleep(1)
+		print(mSerial.message)
+		print('next line')
+    # mSerial.close();
 
 if __name__ == '__main__':
     main();
