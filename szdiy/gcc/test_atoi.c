a32fe3876f77bae8e389a9b59313857b264ca067
Gump Yang <gump.yang@gmail.com>
2018-08-02 23:29:03 +0800

    反复测试并没有出现打印12800的情况

--- szdiy/gcc/test_atoi.c
+++ szdiy/gcc/test_atoi.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	unsigned char buf[4];
+	unsigned char buf[4] = {0, 0, 0, 0};
 	unsigned char buf1[2] = {0x30, 0x31};
 	unsigned int  number = 0;
 
@@ -17,6 +17,8 @@ int main(void)
 	buf[2] = '8';
 	buf[3] = '0';
 	number = atoi(buf);
+	buf1[1] = buf1[1] + 2;
 	printf("test out number=%d\r\n", number);
+	printf("test out buf1=%d\r\n", buf1[1]);
 	return 0;
 }
