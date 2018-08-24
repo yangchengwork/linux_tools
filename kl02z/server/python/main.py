870a92ec6cf15c4e83f6c66ccae4850797a1b8ca
gumpyang <gumpyang@4b97ce20-0248-4398-a9e2-2c177b3f730e>
2018-08-24 14:13:27 +0000

    可以收到数据
    git-svn-id: svn://www.svn999.com/gumpyang.learn@13 4b97ce20-0248-4398-a9e2-2c177b3f730e

--- kl02z/server/python/main.py
+++ kl02z/server/python/main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request;
+import struct;
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -34,13 +36,19 @@ def about():
 
 @app.route('/kl02z', methods=['GET', 'POST'])
 def login():
-    """
     if request.method == 'POST':
-        do_the_login()
+        # print(request.form);
+        reStr = request.form['reStr'];
+        # buf = struct.unpack(reStr, request.form['val']);
+        buf = request.form['val'];
+        buf = struct.unpack(reStr, buf.encode());
+        print(reStr, buf);
+        # do_the_login()
+        return 'The Post page'
     else:
-        show_the_login_form()
-    """
-    return 'The Login Page'
+        # show_the_login_form()
+        print("test kl02z Get");
+        return 'The Get Page'
     
 
 if __name__ == '__main__':
