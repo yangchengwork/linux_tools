88552b65bfd02f58cafa988ad069e323f59f24dd
gumpyang <gumpyang@4b97ce20-0248-4398-a9e2-2c177b3f730e>
2018-08-24 14:13:17 +0000

    尝试输出条数
    git-svn-id: svn://www.svn999.com/gumpyang.learn@4 4b97ce20-0248-4398-a9e2-2c177b3f730e

--- kl02z/client/hardware/python/main.py
+++ kl02z/client/hardware/python/main.py
@@ -10,34 +10,45 @@ COMPATH = "/dev/ttyACM0";
 COMBPS = 115200;
 
 class MSerialPort:
-	message=''
-	def __init__(self,port,buand):
-		self.port=serial.Serial(port,buand)
-		if not self.port.isOpen():
-			self.port.open()
-	def port_open(self):
-		if not self.port.isOpen():
-			self.port.open()
-	def port_close(self):
-		self.port.close()
-	def send_data(self,data):
-		number=self.port.write(data)
-		return number
-	def read_data(self):
-		while True:
-			data=self.port.readline()
-			self.message=data
+    message=''
+    counter=0
+    def __init__(self,port,buand):
+        self.port=serial.Serial(port,buand)
+        if not self.port.isOpen():
+            self.port.open()
+    def port_open(self):
+        if not self.port.isOpen():
+            self.port.open()
+    def port_close(self):
+        self.port.close()
+    def send_data(self,data):
+        number=self.port.write(data)
+        return number
+    def read_data(self):
+        self.counter = 0;
+        while True:
+            data=self.port.readline()
+            # print(data);
+            self.counter += 1
+            self.message=data
 
 def main():
-	mSerial=MSerialPort(COMPATH, COMBPS)
-	_thread.start_new_thread(mSerial.read_data,())
+    mSerial=MSerialPort(COMPATH, COMBPS)
+    _thread.start_new_thread(mSerial.read_data,())
     # thread_read = threading.Thread(target=mSerial.read_data)
     # thread_read.start();
-	while True:
-		# time.sleep(1)
-		print(mSerial.message)
-		print('next line')
-    # mSerial.close();
+    """
+    while True:
+        time.sleep(1)
+        a = input("input:").lower();
+        if a == 'q':
+            break;
+        print(mSerial.message)
+        print('next line')
+    """
+    time.sleep(1);
+    print(mSerial.counter);
+    mSerial.port_close();
 
 if __name__ == '__main__':
     main();
