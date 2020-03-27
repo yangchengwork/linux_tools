8b2e96de1303836cc7938927f3fdff5830e1dae0
Gump Yang <gump.yang@gmail.com>
2020-03-27 20:59:16 +0800

    尝试GET

--- test_go/gin_study/main.go
+++ test_go/gin_study/main.go
@@ -12,5 +12,23 @@ func main() {
         c.String(http.StatusOK, "Who are you?");
     })
 
+    r.GET("/user/:name", func(c *gin.Context) {
+        name := c.Param("name");
+        c.String(http.StatusOK, "Hello %s", name);
+    })
+
+    r.GET("/user/:name/*action", func(c *gin.Context) {
+        name := c.Param("name");
+        action := c.Param("action");
+        msg := name + " is " + action;
+        c.String(http.StatusOK, msg);
+    })
+
+    r.GET("/users", func(c *gin.Context) {
+        name := c.Query("name");
+        role := c.DefaultQuery("role", "teacher");
+        c.String(http.StatusOK, "%s is %s", name, role);
+    })
+
     r.Run(":9080");
 }
